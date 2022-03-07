9ec742105c1be28e898599c18de669313986861b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-07 19:46:33 +0300

    Added a class represents bif buffer

---
+++ BitBuffer.cpp
@@ -0,0 +1,36 @@
+//
+// Created by gunSlaveUnit on 07.03.2022.
+//
+
+#include "BitBuffer.h"
+
+void BitBuffer::append(int value) {
+    int byteIndex = currentBit >> 3;
+    int bitIndex = currentBit & 0b111;
+
+    if (value != 0)
+        bytes[byteIndex] = bytes[byteIndex] | masks[bitIndex];
+    else
+        bytes[byteIndex] = bytes[byteIndex] & ~masks[bitIndex];
+
+    ++currentBit += 1;
+}
+
+BitBuffer::BitBuffer(int size) : size(size), currentBit(0) {
+    int sizeInBytes = size >> 3;
+
+    if(size % 8 > 0)
+        ++sizeInBytes;
+
+    bytes.resize(sizeInBytes);
+}
+
+int BitBuffer::get(int index) {
+    int byteIndex = index >> 3;
+    int bitIndex = index & 0b111;
+    return (bytes[byteIndex] & masks[bitIndex]) != 0 ? 1 : 0;
+}
+
+void BitBuffer::setCurrent(int current) {
+    this->currentBit = current;
+}
---
+++ BitBuffer.h
@@ -0,0 +1,32 @@
+//
+// Created by gunSlaveUnit on 07.03.2022.
+//
+
+#pragma once
+
+#ifndef ADAPVITVEHAFFMAN_BITBUFFER_H
+#define ADAPVITVEHAFFMAN_BITBUFFER_H
+
+#include <cstdint>
+#include <vector>
+
+
+class BitBuffer {
+public:
+    explicit BitBuffer(int size);
+    void append(int value);
+    void setCurrent(int current);
+    int get(int index);
+private:
+    static constexpr int BITES_IN_BYTE = 8;
+    static constexpr unsigned char masks[BITES_IN_BYTE] = {
+            0b00000001, 0b00000010, 0b00000100, 0b00001000,
+            0b00010000, 0b00100000, 0b01000000, 0b10000000
+    };
+    int size;
+    int currentBit{};
+    std::vector<unsigned char> bytes;
+};
+
+
+#endif //ADAPVITVEHAFFMAN_BITBUFFER_H
