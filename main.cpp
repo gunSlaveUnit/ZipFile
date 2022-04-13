eb62a01d49ae3eb8ef242a0c1d462d63ab4f79cc
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-13 14:01:40 +0300

    Created test main window. It works successfully

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+#include <QApplication>
+#include <QMainWindow>
+
 #include "AdaptiveHuffmanCoder.h"
 
 
 int main(int argc, char **argv) {
+    QApplication a(argc, argv);
+    QMainWindow window;
+    window.setFixedSize(300, 200);
+
     auto coder = AdaptiveHuffmanCoder();
     //coder.encode("alice.txt");
-    coder.decode("alice.ahf");
+    //coder.decode("alice.ahf");
+
+    window.show();
+    return QApplication::exec();
+
 }
